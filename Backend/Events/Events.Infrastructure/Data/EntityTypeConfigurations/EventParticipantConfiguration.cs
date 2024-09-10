b84dd3a9ae14ccc2a5cf971544464611c7f6410b
John-Gavr <johngavr19@gmail.com>
2024-09-11 02:20:15 +0300

    Created configurations

---
+++ Backend/Events/Events.Infrastructure/Data/EntityTypeConfigurations/EventParticipantConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Events.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Events.Infrastructure.Data.EntityTypeConfigurations;
+
+public class EventParticipantConfiguration : IEntityTypeConfiguration<EventParticipant>
+{
+    public void Configure(EntityTypeBuilder<EventParticipant> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.FirstName)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(p => p.LastName)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(p => p.Email)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.Property(p => p.DateOfBirth)
+               .IsRequired();
+
+        builder.Property(p => p.RegistrationDate)
+               .IsRequired();
+
+        builder.HasIndex(p => p.UserId).IsUnique(false);
+
+        builder.HasIndex(p => new { p.UserId, p.EventId }).IsUnique();
+    }
+}
