f77ae115bc76012d225757f2891737b3b6e4e29b
John-Gavr <johngavr19@gmail.com>
2024-09-11 02:17:48 +0300

    Created AppDbContext

---
+++ Backend/Events/Events.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Events.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Infrastructure.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Event> Events { get; set; }
+    public DbSet<EventParticipant> EventParticipants { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entities = ChangeTracker.Entries<Event>()
+            .Where(e => e.State == EntityState.Modified)
+            .ToList();
+
+        foreach (var entity in entities)
+        {
+            var eventEntity = entity.Entity;
+            eventEntity.IsFull = eventEntity.Participants.Count >= eventEntity.MaxParticipants;
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
+
