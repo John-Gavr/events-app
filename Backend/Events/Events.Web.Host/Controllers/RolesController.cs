e551331d5a3fe96a47b2520e72914fcdc21093fc
John-Gavr <johngavr19@gmail.com>
2024-09-11 09:49:38 +0300

    Created Controllers

---
+++ Backend/Events/Events.Web.Host/Controllers/RolesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Events.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Events.Application.DTOs.Roles.Requests;
+
+namespace Events.Web.Host.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RolesController : ControllerBase
+{
+    private readonly IRolesService _rolesService;
+
+    public RolesController(IRolesService rolesService)
+    {
+        _rolesService = rolesService;
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetAllRoles()
+    {
+        var roles = _rolesService.GetAllRoles();
+        return Ok(roles);
+    }
+
+    [HttpGet("users/user/roles")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetUserRoles([FromQuery] GetUserRolesRequest request)
+    {
+        var roles = await _rolesService.GetUsersRoleAsync(request);
+        return Ok(roles);
+    }
+
+    [HttpPost("users/user/roles")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> SetUserRole(SetUsersRolesRequest request)
+    {
+        await _rolesService.SetUsersRoleAsync(request);
+        return Ok(new { Message = "Role assigned successfully." });
+    }
+}
---
+++ Backend/Events/Events.Web.Host/Controllers/UserDataController.cs
@@ -0,0 +1,28 @@
+﻿using Events.Application.DTOs.Users.Requests.GetUserDataByEmail;
+using Events.Application.DTOs.Users.Requests.GetUserDataById;
+using Events.Application.DTOs.Users.Responses;
+using Events.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Web.Host.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserDataController : ControllerBase
+{
+    private readonly IUserDataService _userDataService;
+
+    public UserDataController(IUserDataService userDataService) { _userDataService = userDataService; }
+
+    [HttpGet("byEmail")]
+    [Authorize(Policy = "CurrentUserByEmailPolicy")]
+    public async Task<UserDataResponse> GetUserDataByEmailAsync([FromQuery] GetUserDataByEmailRequest request) {
+        return await _userDataService.GetUserDataByEmail(request);
+    }
+    [HttpGet("byId")]
+    [Authorize(Policy = "CurrentUserPolicy")]
+    public async Task<UserDataResponse> GetUserDataByIdAsync([FromQuery] GetUserDataByIdRequest request) {
+        return await _userDataService.GetUserDataById(request);
+    }
+}
