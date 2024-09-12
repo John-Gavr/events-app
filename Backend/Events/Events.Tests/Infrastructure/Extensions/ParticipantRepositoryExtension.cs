47aa9f17538fd136e1318b76ce6019df8a991421
John-Gavr <johngavr19@gmail.com>
2024-09-12 04:23:33 +0300

    Created structure + Implemented tests for repositories

---
+++ Backend/Events/Events.Tests/Infrastructure/Extensions/ParticipantRepositoryExtension.cs
@@ -0,0 +1,21 @@
+﻿using Events.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Tests.Infrastructure.Extensions;
+
+public class ParticipantRepositoryExtension
+{
+    private readonly AppDbContext _context;
+
+    public ParticipantRepositoryExtension(AppDbContext context)
+    {
+        _context = context;
+    }   
+
+    public async void GetParticipantsToEventAsync(int eventId)
+    {
+        var eventEntity = await _context.Events.FirstOrDefaultAsync(e => e.Id == eventId);
+
+        eventEntity!.Participants = _context.EventParticipants.Where(p => p.EventId == eventId).ToList();
+    }
+}
---
+++ Backend/Events/Events.Tests/Infrastructure/Tests/EventRepositoryTests.cs
@@ -0,0 +1,163 @@
+﻿using Events.Core.Entities;
+using Events.Core.Entities.Exceptions;
+using Events.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Tests.Infrastructure.Tests;
+
+public class EventRepositoryTests : InfrastructureTestBase
+{
+    private readonly EventRepository _repository;
+
+    public EventRepositoryTests()
+    {
+        _repository = new EventRepository(_context, _unitOfWorkMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllEventsAsync_ShouldReturnPaginatedEvents()
+    {
+        var result = await _repository.GetAllEventsAsync(1, 10);
+
+        Assert.NotNull(result);
+        Assert.True(result.Any());
+    }
+
+    [Fact]
+    public async Task GetEventByIdAsync_ShouldReturnEvent_WhenEventExists()
+    {
+        var eventId = 1; 
+
+        var result = await _repository.GetEventByIdAsync(eventId);
+
+        Assert.NotNull(result);
+        Assert.Equal(result, _context.Events.Find(eventId));
+    }
+
+    [Fact]
+    public async Task GetEventByIdAsync_ShouldReturnNull_WhenEventDoesNotExist()
+    {
+        var eventId = 999;
+
+        var result = await _repository.GetEventByIdAsync(eventId);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task AddEventAsync_ShouldAddNewEvent()
+    {
+        var newEvent = new Event
+        {
+            Id = 3,
+            Name = "New Event",
+            Description = "Test description",
+            EventDateTime = DateTime.Parse("2024-10-10"),
+            Location = "Test Location",
+            MaxParticipants = 100
+        };
+
+        await _repository.AddEventAsync(newEvent);
+        var result = await _context.Events.FirstOrDefaultAsync(e => e.Name == "New Event");
+
+        Assert.NotNull(result);
+        Assert.Equal("New Event", result.Name);
+    }
+
+    [Fact]
+    public async Task UpdateEventAsync_ShouldUpdateExistingEvent()
+    {
+        var updatedValues = new
+        {
+            Name = "Updated Event",
+            Date = DateTime.Parse("2025-01-25"),
+            Location = "Updated Location",
+            MaxParticipants = 150,
+            Category = "Updated Category",
+            Description = "Updated Description"
+        };
+
+        var eventToUpdate = await _context.Events.FindAsync(2);
+        Assert.NotNull(eventToUpdate); 
+
+        eventToUpdate!.Name = updatedValues.Name;
+        eventToUpdate.EventDateTime = updatedValues.Date;
+        eventToUpdate.Location = updatedValues.Location;
+        eventToUpdate.MaxParticipants = updatedValues.MaxParticipants;
+        eventToUpdate.Category = updatedValues.Category;
+        eventToUpdate.Description = updatedValues.Description;
+
+        await _repository.UpdateEventAsync(eventToUpdate);
+        var updatedEvent = await _context.Events.FirstOrDefaultAsync(e => e.Id == eventToUpdate.Id);
+
+        Assert.NotNull(updatedEvent);
+        Assert.Equal(updatedValues.Name, updatedEvent!.Name);
+        Assert.Equal(updatedValues.Date, updatedEvent.EventDateTime);
+        Assert.Equal(updatedValues.Location, updatedEvent.Location);
+        Assert.Equal(updatedValues.MaxParticipants, updatedEvent.MaxParticipants);
+        Assert.Equal(updatedValues.Category, updatedEvent.Category);
+        Assert.Equal(updatedValues.Description, updatedEvent.Description);
+    }
+
+    [Fact]
+    public async Task DeleteEventAsync_ShouldRemoveEvent_WhenEventExists()
+    {
+        var eventId = 2; 
+
+        await _repository.DeleteEventAsync(eventId);
+        var result = await _context.Events.FindAsync(eventId);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task DeleteEventAsync_ShouldThrowNotFoundException_WhenEventDoesNotExist()
+    {
+        var eventId = 999;
+
+        await Assert.ThrowsAsync<NotFoundException>(() => _repository.DeleteEventAsync(eventId));
+    }
+
+    [Fact]
+    public async Task GetEventsByCriteriaAsync_ShouldReturnEventsByDate()
+    {
+        var date = DateTime.Parse("2024-10-10"); 
+
+        var result = await _repository.GetEventsByCriteriaAsync(date: date);
+
+        Assert.NotNull(result);
+        Assert.All(result, e => Assert.Equal(date, e.EventDateTime.Date));
+    }
+    [Fact]
+    public async Task GetEventsByCriteriaAsync_ShouldReturnEventsByLocation()
+    {
+        var location = "Location of the second event"; 
+
+        var result = await _repository.GetEventsByCriteriaAsync(location: location);
+
+        Assert.NotNull(result);
+        Assert.All(result, e => Assert.Equal(location, e.Location));
+    }
+
+    [Fact]
+    public async Task AddEventImageAsync_ShouldUpdateEventImage()
+    {
+        var eventId = 1;
+        var imageBytes = new byte[] { 1, 2, 3, 4 };
+
+        await _repository.AddEventImageAsync(eventId, imageBytes);
+        var updatedEvent = await _context.Events.FindAsync(eventId);
+
+        Assert.NotNull(updatedEvent);
+        Assert.Equal(imageBytes, updatedEvent.Image);
+    }
+    
+    [Fact]
+    public async Task AddEventImageAsync_ShouldhrowNotFoundException_WhenEventDoesNotExist()
+    {
+        var eventId = 99;
+        var imageBytes = new byte[] { 1, 2, 3, 4 };
+
+        await Assert.ThrowsAsync<NotFoundException>(() => _repository.AddEventImageAsync(eventId, imageBytes));
+    }
+}
