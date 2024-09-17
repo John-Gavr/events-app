4cf99f7f277ce76d6bfb4f81209a9fdd2282ca1e
John-Gavr <johngavr19@gmail.com>
2024-09-18 02:36:26 +0300

    Created EventModel

---
+++ Frontend/events-frontend/app/Models/Event.tsx
@@ -0,0 +1,11 @@
+interface eventObject {
+    id: string,
+    name: string,
+    description: string,
+    eventDateTime: Date,
+    location: string,
+    category: string,
+    currentCountOfParticipants: number,
+    maxParticipants: number,
+    image: string
+}
