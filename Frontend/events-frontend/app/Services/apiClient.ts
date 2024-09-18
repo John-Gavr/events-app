1e2a973af35f28cae372ea5945d40b830babc3e1
John-Gavr <johngavr19@gmail.com>
2024-09-18 21:59:37 +0300

    fixed login and register pages

--- Frontend/events-frontend/app/Services/apiClient.ts
+++ Frontend/events-frontend/app/Services/apiClient.ts
@@ -1,10 +1,9 @@
-// api.ts
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function apiFetch<T>(
+export async function apiFetch(
   endpoint: string,
   options?: RequestInit
-): Promise<T> {
+): Promise<Response> {
   const response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
     credentials: 'include',
@@ -14,9 +13,11 @@ export async function apiFetch<T>(
     },
   });
 
+  console.log("fetch works", response);
+
   if (!response.ok) {
-    throw new Error('request error');
+    throw new Error(`Request failed with status ${response.status}`);
   }
 
-  return response.json();
+  return response;
 }
